3756e78d1b9492ae7f3933d82ceb68e22e5bf72f
‘Shelby <sls1881@gmail.com>
2021-03-04 18:01:32 -0800

    four tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -82,104 +82,47 @@ describe('app routes', () => {
       expect(data.body).toEqual(expectation);
     });
 
-    // //test delete favorite by ID
-    // test('Delete a single character by id', async () => {
+    //test delete favorite by ID
+    test('Delete a single character by id', async () => {
 
-    //   const expectation = {
-    //     id: 2,
-    //     yelp_id: 'dfsdfdsf7',
-    //     name: 'Screen Door',
-    //     image_url: 'https://cdn.vox-cdn.com/thumbor/1lSaieb0-C2VSB752pnWk0FdLhg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19569065/Studio_Session_080_3_1_.jpg',
-    //     rating: '5',
-    //     price: '$$',
-    //     user_id: 2,
-    //   };
+      const expectation = {
+        id: 2,
+        yelp_id: 'dfsdfdsf7',
+        name: 'Screen Door',
+        image_url: 'https://cdn.vox-cdn.com/thumbor/1lSaieb0-C2VSB752pnWk0FdLhg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19569065/Studio_Session_080_3_1_.jpg',
+        rating: '5',
+        price: '$$',
+        user_id: 2,
+      };
 
-    //   const data = await fakeRequest(app)
-    //     .delete('/api/favorites/2')
-    //     .set('Authorization', token);
-    //   // .expect('Content-Type', /json/)
-    //   // .expect(200);
+      const data = await fakeRequest(app)
+        .delete('/api/favorites/2')
+        .set('Authorization', token);
+      // .expect('Content-Type', /json/)
+      // .expect(200);
 
-    //   expect(data.body).toEqual(expectation);
+      expect(data.body).toEqual(expectation);
 
 
-    //   const nothing = await fakeRequest(app)
-    //     .get('/api/favorites/2')
-    //     .set('Authorization', token);
-    //   // .expect('Content-Type', /json/)
-    //   // .expect(200);
+      const nothing = await fakeRequest(app)
+        .get('/api/favorites/2')
+        .set('Authorization', token);
+      // .expect('Content-Type', /json/)
+      // .expect(200);
 
-    //   expect(nothing.body).toEqual('');
-    // });
+      expect(nothing.body).toEqual({});
+    });
 
     //test get yelp review
-    // test('It should return a review from yelp', () => {
+    test('It should return a review from yelp', async () => {
 
-    //   const expectation = [
-    //     {
-    //       yelp_id: 'Ys42wLKqrflqmtqkgqOXgA',
-    //       name: 'Luc Lac',
-    //       image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/azr6sD6VeJbdaiO2aKvSsw/o.jpg',
-    //       rating: '4.5',
-    //       price: '$$',
-    //     }
-    //   ];
+      await fakeRequest(app)
+        .get('/api/search?location=portland')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-    //   const actual = [
-    //     {
-    //       'id': 'Ys42wLKqrflqmtqkgqOXgA',
-    //       'alias': 'luc-lac-portland-7',
-    //       'name': 'Luc Lac',
-    //       'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/azr6sD6VeJbdaiO2aKvSsw/o.jpg',
-    //       'is_closed': false,
-    //       'url': 'https://www.yelp.com/biz/luc-lac-portland-7?adjust_creative=CQE1JjgHx0oM_A9JTQhg7A&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=CQE1JjgHx0oM_A9JTQhg7A',
-    //       'review_count': 3205,
-    //       'categories': [
-    //         {
-    //           'alias': 'vietnamese',
-    //           'title': 'Vietnamese'
-    //         },
-    //         {
-    //           'alias': 'tapasmallplates',
-    //           'title': 'Tapas/Small Plates'
-    //         },
-    //         {
-    //           'alias': 'cocktailbars',
-    //           'title': 'Cocktail Bars'
-    //         }
-    //       ],
-    //       'rating': 4.0,
-    //       'coordinates': {
-    //         'latitude': 45.516868,
-    //         'longitude': -122.675447
-    //       },
-    //       'transactions': [
-    //         'pickup',
-    //         'delivery'
-    //       ],
-    //       'price': '$$',
-    //       'location': {
-    //         'address1': '835 SW 2nd Ave',
-    //         'address2': null,
-    //         'address3': '',
-    //         'city': 'Portland',
-    //         'zip_code': '97204',
-    //         'country': 'US',
-    //         'state': 'OR',
-    //         'display_address': [
-    //           '835 SW 2nd Ave',
-    //           'Portland, OR 97204'
-    //         ]
-    //       },
-    //       'phone': '+15032220047',
-    //       'display_phone': '(503) 222-0047',
-    //       'distance': 1312.1776320869053
-    //     }
-    //   ];
-
-    //   expect(actual).toEqual(expectation);
-    // });
+    });
 
   });
 });
